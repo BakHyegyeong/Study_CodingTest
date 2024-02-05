339fa683a7c0a15e5829b198dda76353a2cfe0aa
Bak hyegyeong <83454423+BakHyegyeong@users.noreply.github.com>
2024-02-05 09:11:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 148 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+//		ArrayList<Integer> sum = new ArrayList<Integer>();
+		
+		String input = br.readLine();
+
+		bw.write(input.length() + " ");
+		
+		bw.close();
+	}
+
+}
