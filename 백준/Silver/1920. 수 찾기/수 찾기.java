285eaa5c009c6f91fbb6ac56ae833f905da5d188
Bak hyegyeong <83454423+BakHyegyeong@users.noreply.github.com>
2024-10-17 16:27:02 +0900

    [Silver IV] Title: 수 찾기, Time: 584 ms, Memory: 47280 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+// 코테는 java.util.* 로!
+// 자바는 java.io.* 로!
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] array = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0 ; i < n ; i++){
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(array);
+
+		int m = Integer.parseInt(br.readLine());	
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i=0 ; i < m ; i++){
+			int num = Integer.parseInt(st.nextToken());
+
+			bw.write(search(array, num, 0, n-1) + "\n");
+		}	
+		
+		// bw.write(money + "");
+
+		bw.flush();
+		bw.close();
+		
+	}
+
+	public static int search(int[] array, int key, int low, int high){
+		int mid;
+
+		while(low <= high){
+			mid = (low + high)/2;
+
+			if(key == array[mid]){
+				return 1;
+			}else if(key < array[mid]){
+				high = mid-1;
+			}else{
+				low = mid+1;
+			}
+		}
+
+		return 0;
+	}
+
+	
+
+}
